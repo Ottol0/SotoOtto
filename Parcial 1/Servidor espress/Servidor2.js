5a9096e663522ad016638af9b98e311f20efa3d7
Otto10 <ottomaldonado01@gmail.com>
2022-09-22 09:22:17 -0500

    trabajoclase

--- Parcial 1/Servidor espress/Servidor2.js
+++ Parcial 1/Servidor espress/Servidor2.js
@@ -2,17 +2,35 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+app.use(express.text())
+app.use(express.json())
 
 app.use(cors({origin:'http://127.0.0.1:5500'}))
-app.get('/',(req,res)=>{
-    res.send('Servidor express')
-})
 
-app.listen(8083,()=>{console.log('Servidor funcinal')})
 
 app.get('/', (req,res)=> {
     res.sendFile('/static/index.html',{root:__dirname})
 })
+
+
+app.post('/json', (req, res)=> {
+    console.log(req.body.nombre)
+    let cadena = "Bienvenido:" +req.body.nombre+""+req.body.apellido
+    res.json({saludo:cadena})
+})
+
+app.get('/mayusculas/:cadena', (req,res)=> {
+    console.log(req.params)
+    res.send(req.params)
+})
+
+app.get('/suma', (req,res)=> {
+    console.log(req.query)
+    let suma = parseInt(req.query.x)+parseInt(req.query.y)
+    res.send(`La suma es ${suma}`)
+})
+
+app.listen(8083,()=>{console.log('Servidor funcinal')})
 
 app.use( (req,res)=> {
     res.sendFile('/static/404.html',{root:__dirname})
