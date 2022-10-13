9ed3f830ed336ca3aba5a7eb17b33c65721ea03e
Otto10 <ottomaldonado01@gmail.com>
2022-10-12 20:50:32 -0500

    modulos

---
+++ Parcial 2/modeloESM/modelodinamico/cadenaesmdinamica.js
@@ -0,0 +1,10 @@
+
+export function pasarMayusculas(cadena){
+    return cadena.toUpperCase();
+}
+export function quitarEspacios(cadena){
+    return cadena.replace(/ /g,"");
+}
+export function obtenerLongitud(cadena){
+    return cadena.length;
+}
