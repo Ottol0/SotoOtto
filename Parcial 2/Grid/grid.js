3baf442fd2e7f96808b85442f2c4940efd1f37e3
Otto10 <ottomaldonado01@gmail.com>
2022-11-09 14:29:53 -0600

    Yaml

--- Parcial 2/Grid/grid.js
+++ Parcial 2/Grid/grid.js
@@ -2,8 +2,8 @@ const grid = new gridjs.Grid({
     columns: ['Id', 'Nombre', 'Apellido'],
     server: {
       url: 'http://localhost:8081',
-      then: data => data.results.map(movie => 
-        [movie.title, movie.director, movie.producer]
+      then: data => data.map(personasxd => 
+        [personasxd.id, personasxd.nombre, personasxd.apellido]
       )
     } 
   }).render(document.getElementById("wrapper"));
