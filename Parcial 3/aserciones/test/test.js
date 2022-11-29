c8e5030453f84df388b787524df76311158116fd
Otto10 <ottomaldonado01@gmail.com>
2022-11-29 09:30:33 -0600

    aserciones

---
+++ Parcial 3/aserciones/test/test.js
@@ -0,0 +1,29 @@
+let funciones = require('../src/funciones.js');
+let chai = require('chai');
+let shoul = chai.should();
+let expect = chai.expect;
+let assert = chai.assert;
+
+describe('Pruebas de la funcion potencial', function(){
+    it('Deberia regresar un 8',function(){
+        let resultado = funciones.potencia(2,3);
+        resultado.should.be.a('number');
+        resultado.should.equal(8)    
+    })
+})
+
+describe('Pruebas de la funcion potencial', function(){
+    it('Deberia regresar un 8',function(){
+        let resultado = funciones.potencia(2,3);
+        assert.typeOf(resultado, 'number');
+        assert.equal(resultado,8)    
+    })
+})
+
+describe('Pruebas de la funcion potencial', function(){
+    it('Deberia regresar un 8',function(){
+        let resultado = funciones.potencia(2,3);
+        expect(resultado).to.be.a('number');
+        expect(resultado).to.equal(8)    
+    })
+})
